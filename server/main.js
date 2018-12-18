2157cbe3672853b405aff8b0e14289d9a362bba9
grexess <grexess@googlemail.com>
2018-12-18 20:47:15 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -16,6 +16,39 @@ Meteor.publish('votings', function () {
   return Votings.find({ votedBy: Meteor.user().emails[0].address });
 });
 
+process.env.MAIL_URL = "smtp://user:pass@yourservice.com:587";
+
+/* Account reistration mail stuff */
+Accounts.config({
+  sendVerificationEmail: true
+});
+
+Accounts.emailTemplates.siteName = 'AwesomeSite';
+Accounts.emailTemplates.from = 'admin@hexenhaus.com';
+
+Accounts.emailTemplates.enrollAccount.subject = (user) => {
+  return `Welcome to Awesome Town, ${user.profile.name}`;
+};
+
+Accounts.emailTemplates.enrollAccount.text = (user, url) => {
+  return 'You have been selected to participate in building a better future!'
+    + ' To activate your account, simply click the link below:\n\n'
+    + url;
+};
+
+Accounts.emailTemplates.resetPassword.from = () => {
+  // Overrides the value set in `Accounts.emailTemplates.from` when resetting
+  // passwords.
+  return 'AwesomeSite Password Reset <no-reply@example.com>';
+};
+Accounts.emailTemplates.verifyEmail = {
+   subject() {
+      return "Activate your account now!";
+   },
+   text(user, url) {
+      return `Hey ${user}! Verify your e-mail by following this link: ${url}`;
+   }
+};
 
 Votings.allow({
   'insert': function (userId, doc) {
